1ceb44f4da09e8ca75f0d466ab8ca23dc4a7f829
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-06-07 01:54:54 +0530

    mini calender

---
+++ Mini calender/index.js
@@ -0,0 +1,10 @@
+const monthNameE1 = document.getElementById("month-name");
+const dayNameE1 = document.getElementById("day-name");
+const dayNumberE1 = document.getElementById("day-number");
+const yearE1 = document.getElementById("year");
+
+const date = new Date();
+
+const month = date.getMonth();
+monthNameE1.innerText= date.toLocaleString("en" ,{month:"long"});
+
