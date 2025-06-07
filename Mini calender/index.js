36b21430aa33df473ccfa4a436ccbd935290f210
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-06-07 20:57:49 +0530

    Active date updated

--- Mini calender/index.js
+++ Mini calender/index.js
@@ -8,3 +8,6 @@ const date = new Date();
 const month = date.getMonth();
 monthNameE1.innerText= date.toLocaleString("en" ,{month:"long"});
 
+dayNameE1.innerText = date.toLocaleString("en" ,{weekday:"long"});
+dayNumberE1.innerText = date.getDate();
+yearE1.innerText = date.getFullYear();
